58c468921d3e3a7c0716fe5e4e84db48c1d7482e
rajaram <govindjsg19@gmail.com>
2023-08-20 00:57:20 +0100

    Initial commit

---
+++ app/src/main/java/com/greet/viewsapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.greet.viewsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.*
+import com.greet.viewsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // Step 2: Declaring Views
+        val edt : EditText = findViewById(R.id.edt)
+        val btn = findViewById<Button>(R.id.btn)
+
+        // Step 3: Adding functionalities
+        btn.setOnClickListener(){
+            var entered_text = edt.text
+
+            Toast.makeText(
+                this,
+            "Hello "+entered_text,
+            Toast.LENGTH_LONG)
+                .show()
+        }
+
+
+
+    }
+}
